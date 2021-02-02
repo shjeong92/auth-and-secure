f802cb2e6cac870df936c984f301c8c310770fb9
Sanghyuk Jeong <75003424+shjeong92@users.noreply.github.com>
2021-02-02 15:36:34 +0900

    Use md5

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser =require("body-parser");
 const ejs = require("ejs");
 const app = express();
 const mongoose = require("mongoose");
-const encrypt = require('mongoose-encryption');
+const md5 = require("md5");
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -18,7 +18,8 @@ const userSchema = new mongoose.Schema ({
 });
 console.log(process.env.SECRET);
 
-userSchema.plugin(encrypt, { secret : process.env.SECRET, requireAuthenticationCode: false, encryptedFields : ["password"]});
+console.log(md5("123456"));
+
 
 const User = new mongoose.model("User",userSchema);
 
@@ -44,7 +45,7 @@ app.post("/register", function(req, res) {
 
   const newUser = User ({
     email : req.body.username,
-    password :  req.body.password
+    password :  md5(req.body.password)
   });
   newUser.save(function(err){
     if(err){
@@ -58,7 +59,7 @@ app.post("/register", function(req, res) {
 
 app.post("/login", function(req, res) {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email : username}, function(err, foundUser){
     if(err){
